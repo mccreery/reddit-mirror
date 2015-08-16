27cc8ba8905871903341c84f9ca6c1e0f6c57f49
alkorin452 <alkorin452@gmail.com>
2015-08-15 21:53:54 -0400

    Initial commit

---
+++ RedditSaved.py
@@ -0,0 +1,28 @@
+import praw
+
+saved = []
+save_dict = {}
+r = praw.Reddit(user_agent = "")
+username = raw_input(">>Username:")
+password = raw_input(">>Password:")
+subreddit = raw_input(">>Subreddit (without /r/)")
+
+r.login(username,password)
+
+for post in r.user.get_saved():
+
+    post_name = str(post)
+
+    try:
+        semi = post_name.index(':')
+        post_name = post_name[semi+2:]
+    except Exception, CannotFind:
+        pass
+
+    sub = str(post.subreddit.display_name)
+    package = '[' + sub + ']' + ' ' + post_name
+    link = post.permalink
+    print package
+    r.submit(subreddit, package, url = link)
+    post.unsave()
+
